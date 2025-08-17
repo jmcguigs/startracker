4ce0e48bf43d10900caf36fe0f0556bf868745e3
jmcguigs <jemcguigan@outlook.com>
2025-08-17 14:59:54 -0700

    derivative hdl & test

--- hdl/histogram_derivative.sv
+++ hdl/histogram_derivative.sv
@@ -1,18 +1,18 @@
 `timescale 1ns/1ps
 
 module histogram_derivative(
-    input logic [7:0][15:0] i_histogram,
-    output logic [7:0][15:0] o_derivative
+    input logic [15:0] i_histogram [0:255],
+    output logic [15:0] o_derivative [0:255]
 );
 
     // take the discrete derivative of the histogram
 
     always_comb begin
         for (int i = 0; i < 256; i++) begin
-            if (i < 255) begin
-                o_derivative[i] = i_histogram[i] - i_histogram[i - 1]; // Calculate derivative for elements 1 to 254
+            if (i == 0) begin
+                o_derivative[i] = 0;
             end else begin
-                o_derivative[i] = 0; // Last element has no next element to subtract from
+                o_derivative[i] = i_histogram[i] - i_histogram[i - 1];
             end
         end
     end
---
+++ tests/test_histogram_derivative.py
@@ -0,0 +1,92 @@
+import os
+import sys
+import random
+from pathlib import Path
+
+import cocotb
+from cocotb.types import LogicArray, Range
+from cocotb.clock import Timer
+from cocotb.runner import get_runner
+
+
+FILE_NAME = "histogram_derivative"
+
+@cocotb.test()
+async def derivative_zero_test(dut):
+    """Test that histogram_derivative.sv computes the derivative correctly"""
+
+    for i in range(256):
+        dut.i_histogram[i].value = i
+
+    await Timer(5, units="ns")
+    for i in range(256):
+        expected_derivative = i - (i - 1) if i > 0 else i
+        assert dut.o_derivative[i].value == expected_derivative, (
+            f"Derivative at index {i} was incorrect: "
+            f"expected {expected_derivative}, got {dut.o_derivative[i].value}"
+        )
+
+
+@cocotb.test()
+async def derivative_nonzero_test(dut):
+    """Test that histogram_derivative.sv computes the derivative correctly"""
+
+    for i in range(256):
+        dut.i_histogram[i].value = i
+
+    await Timer(5, units="ns")
+    for i in range(256):
+        expected_derivative = i if i == 0 else i - (i - 1)
+        assert dut.o_derivative[i].value == expected_derivative, (
+            f"Derivative at index {i} was incorrect: "
+            f"expected {expected_derivative}, got {dut.o_derivative[i].value}"
+        )
+
+
+@cocotb.test()
+async def derivative_negative_test(dut):
+    """Test that histogram_derivative.sv computes the derivative correctly"""
+
+    for i in range(256):
+        dut.i_histogram[i].value = 255 - i
+
+    await Timer(5, units="ns")
+    for i in range(256):
+        expected_derivative = -1 * i if i == 0 else -1 * (i - (i - 1))
+        assert dut.o_derivative[i].value.signed_integer == expected_derivative, (
+            f"Derivative at index {i} was incorrect: "
+            f"expected {expected_derivative}, got {dut.o_derivative[i].value}"
+        )
+
+
+def test_histogram_derivative_runner():
+    """
+    Simulate the histogram derivative module using cocotb.
+    """
+    sim = os.getenv("SIM", "icarus")
+
+    proj_path = Path(__file__).resolve().parent.parent
+    # equivalent to setting the PYTHONPATH environment variable
+    sys.path.append(str(proj_path / "model"))
+
+    sources = [proj_path / "hdl" / f"{FILE_NAME}.sv"]
+
+    build_test_args = []
+
+    # equivalent to setting the PYTHONPATH environment variable
+    sys.path.append(str(proj_path / "tests"))
+
+    runner = get_runner(sim)
+    runner.build(
+        sources=sources,
+        hdl_toplevel=FILE_NAME,
+        always=True,
+        build_args=build_test_args
+    )
+    runner.test(
+        hdl_toplevel=FILE_NAME, test_module="test_histogram_derivative", test_args=build_test_args
+    )
+
+
+if __name__ == "__main__":
+    test_histogram_derivative_runner()
